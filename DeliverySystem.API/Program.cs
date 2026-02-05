23b6e4fddf8fdb87f5071c466ba2d46fb852c93f
Adrian-Cealic <adrian.cealic@gmail.com>
2026-02-05 10:12:38 +0200

    add webapi project with swagger and CORS config

---
+++ DeliverySystem.API/Program.cs
@@ -0,0 +1,53 @@
+using DeliverySystem.Infrastructure.Notifications;
+using DeliverySystem.Infrastructure.Repositories;
+using DeliverySystem.Interfaces;
+using DeliverySystem.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Delivery Management System API",
+        Version = "v1",
+        Description = "REST API for the Delivery Management System - Laborator 1"
+    });
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+builder.Services.AddSingleton<ICustomerRepository, InMemoryCustomerRepository>();
+builder.Services.AddSingleton<IOrderRepository, InMemoryOrderRepository>();
+builder.Services.AddSingleton<ICourierRepository, InMemoryCourierRepository>();
+builder.Services.AddSingleton<IDeliveryRepository, InMemoryDeliveryRepository>();
+builder.Services.AddSingleton<INotificationService, ConsoleNotificationService>();
+
+builder.Services.AddScoped<OrderService>();
+builder.Services.AddScoped<DeliveryService>();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Delivery System API v1");
+});
+
+app.UseCors("AllowReactApp");
+
+app.UseMiddleware<DeliverySystem.API.Middleware.ErrorHandlingMiddleware>();
+
+app.MapControllers();
+
+app.Run();
